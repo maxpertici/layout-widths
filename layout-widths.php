41a35c74b99d8a8bd29075fb0037867f9798bafe
Maxime Pertici <maxime@whodunit.fr>
2025-10-01 14:35:45 +0200

    Mise à jour de la description du plugin pour préciser le support des largeurs de mise en page personnalisées dans l'éditeur de blocs.

--- layout-widths.php
+++ layout-widths.php
@@ -2,13 +2,13 @@
 /*
  * Plugin Name:       Layout Widths
  * Plugin URI:        https://maxpertici.fr
- * Description:       —
+ * Description:       Add support for custom layout widths in the block editor.
  * Version:           0.1.0
  * Author:            @maxpertici
  * Author URI:        https://maxpertici.fr
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Update URI:        https://maxpertici.fr/layout-widths/
+ * Update URI:        https://maxpertici.fr/#layout-widths
  * Text Domain:       layout-widths
  * Domain Path:       /languages
  */
